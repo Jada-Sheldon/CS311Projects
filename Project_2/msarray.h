7333b303540ca70c595ad10a5173896c0dafd30b
Jada Sheldon <jmsheldon2@yahoo.com>
2020-09-17 21:19:38 -0800

    figuring out stuff

--- Project_2/msarray.h
+++ Project_2/msarray.h
@@ -2,8 +2,12 @@
 #ifndef MSARRAY_H
 #define MSARRAY_H
 
+#ifndef MSARRAY_H
+#define MSARRAY_H
+
 #include <cstddef>
 #include <utility>
+#include <algorithm>
 
 template <typename T>
 class MSArray {
@@ -17,31 +21,29 @@ public:
 	}
 
 	// explicit one parameter constructor
-	explicit MSArray(const size_type& size) : _arrayptr(new value_type[size]), _size(size) {
+	explicit MSArray(size_type size) : _arrayptr(new value_type[size]), _size(size) { // was reference to const
 	}
 
 	// two parameter constructor
 	// TODO figure out how to set the whole array to the same value
 	// I think done
-	MSArray(const size_type& size, value_type value) : _arrayptr(new value_type[size]), _size(size) {
+	MSArray(size_type size, value_type value) : _arrayptr(new value_type[size]), _size(size) { // size_type was reference to const
 		// changing to same value for all indexes of array
-		for (int i = 0; i <= _size; i++) {
+		for (int i = 0; i < _size; i++) {
 			_arrayptr[i] = value;
 		}
 	}
 	//copy ctor
-	MSArray(const MSArray& original) noexcept : _arrayptr(new value_type[original.size()]), _size(original.size()) 
+	MSArray(const MSArray& original) noexcept : _arrayptr(new value_type[original.size()]), _size(original.size())
 	{
-		for (int i = 0; i >= _size; i++) {
-			_arrayptr[i] = original[i];
-		}
+		std::copy(original.begin(), original.end(), _arrayptr);
 	}
 
 	//move ctor
-	MSArray(MSArray&& other) noexcept : _arrayptr(other._arrayptr), _size(other.size())  
+	MSArray(MSArray&& other) noexcept : _arrayptr(other._arrayptr), _size(other.size())
 	{
 		// move data from other to new array
-		other._arrayptr = new value_type[0];
+		other._arrayptr = nullptr;
 		other._size = 0;
 		// set other array to a valid value so the destructor still works
 	}
@@ -49,31 +51,14 @@ public:
 	//copy assignment
 	MSArray& operator=(const MSArray& rhs) {
 		MSArray copy_of_rhs(rhs);
-		swap(copy_of_rhs);
+		mswap(copy_of_rhs);
 		return *this;
 	}
 	//move assignment
 	MSArray& operator=(MSArray&& rhs) noexcept
 	{
-		swap(rhs);
+		mswap(rhs);
 		return *this;
-	}
-
-
-
-	// size of array
-	size_type& size() {
-		return _size;
-	}
-
-
-	//TODO: Make member funtion begin
-	value_type* begin() {
-		return _arrayptr;
-	}
-	//TODO: Make member funtion end
-	value_type* end() {
-		return (_arrayptr + _size);
 	}
 
 	// destructor dealicates memory
@@ -88,12 +73,27 @@ public:
 		return _arrayptr[index];
 	}
 
-	const value_type& operator[] (size_type index) const {
-		return _arrayptr[index];
+	//TODO: Make member funtion begin
+	value_type* begin() {
+		return _arrayptr;
+	}
+	//TODO: Make member funtion end
+	value_type* end() {
+		return (_arrayptr + _size);
 	}
 
+
+
+
+
+	// size of array
 	const size_type size() const {
 		return _size;
+	}
+
+
+	const value_type& operator[] (size_type index) const {
+		return _arrayptr[index];
 	}
 
 	const value_type* begin() const {
@@ -109,31 +109,41 @@ private:
 	value_type* _arrayptr;
 	size_type _size;
 
-	void swap(MSArray& other) noexcept {
-		swap(_arrayptr, other._arrayptr);
-		swap(_size, other._size);
+	void mswap(MSArray& other) noexcept {
+		std::swap(_arrayptr, other._arrayptr);
+		std::swap(_size, other._size);
 	}
 };
+
+
+
+
+//TODO: Operator ==
+template<typename T>
+bool operator==(const MSArray<T>& lhs, const MSArray<T>& rhs) {
+	if (lhs.size() == rhs.size()) {
+		for (int i = 0; i < lhs.size(); ++i) {
+			if (lhs[i] != rhs[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
+
+// TODO: Operator !=
+template<typename T>
+bool operator!=(const MSArray<T>& lhs, const MSArray<T>& rhs) {
+	return !(lhs == rhs);
+}
 
 
 template<typename T>
 //TODO: Operators < <= > >=
 bool operator<(const MSArray<T>& lhs, const MSArray<T>& rhs) {
-	int i = 0;
-	while (true) {
-		if (lhs[i] == rhs[i]) {
-			if (i == lhs.size() || i == rhs.size()) {
-				return (lhs.size() < rhs.size());
-			}
-			continue;
-		}
-		else if (lhs[i] < rhs[i]) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+	return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
 }
 
 template<typename T>
@@ -151,16 +161,6 @@ bool operator<= (const MSArray<T>& lhs, const MSArray<T>& rhs) {
 	return !(lhs > rhs);
 }
 
-//TODO: Operator ==
-template<typename T>
-bool operator==(const MSArray<T>& lhs, const MSArray<T>& rhs) {
-	return (!(lhs < rhs) && !(rhs < lhs));
-}
-
-// TODO: Operator !=
-template<typename T>
-bool operator!=(const MSArray<T>& lhs, const MSArray<T>& rhs) {
-	return !(!(lhs < rhs) && !(rhs < lhs));
-}
 
 #endif
+
